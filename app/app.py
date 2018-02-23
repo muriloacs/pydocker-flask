137566d8189f5b742a039c311f89d5ef66c6e868
Murilo Santiago <muriloacs@yahoo.com.br>
2018-02-23 15:25:57 -0300

    Redis: linking containers

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import redis
 
+# App
 app = Flask(__name__)
+
+# Redis
+r = redis.StrictRedis(host='redis', port=6379, db=0)
+r.set('place', 'pyWorld')
 
 
 @app.route('/')
 def hello_word():
-    return 'Hello World!!\n'
+    return 'Hello {0}!!\n'.format(r.get('place'))
 
 
 if __name__ == '__main__':
